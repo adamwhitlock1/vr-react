0a1f02571d101e24c9ca4027f1fcf0b3f8ae25d4
Adam Whitlock <adam.whitlock@adhocteam.us>
2024-04-27 14:11:07 -0600

    add babylon core and hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,67 @@
-export default function Index() {
-  return <div>Hello World</div>;
-}
+import React from "react";
+import * as BABYLON from "@babylonjs/core"; // uses above component in same directory
+
+import SceneComponent from "babylonjs-hook"; // if you install 'babylonjs-hook' NPM.
+
+let box;
+
+const onSceneReady = (scene) => {
+  console.log(BABYLON.StandardMaterial);
+  // This creates and positions a free camera (non-mesh)
+  const camera = new BABYLON.FreeCamera(
+    "camera1",
+    new BABYLON.Vector3(0, 5, -10),
+    scene,
+  );
+
+  // This targets the camera to scene origin
+  camera.setTarget(BABYLON.Vector3.Zero());
+
+  const canvas = scene.getEngine().getRenderingCanvas();
+
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
+
+  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+  const light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(0, 1, 0),
+    scene,
+  );
+
+  // Default intensity is 1. Let's dim the light a small amount
+  light.intensity = 0.7;
+
+  // Our built-in 'box' shape.
+  box = BABYLON.MeshBuilder.CreateBox("box", { size: 2 }, scene);
+
+  // Move the box upward 1/2 its height
+  box.position.y = 1;
+
+  // Our built-in 'ground' shape.
+  BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+};
+
+/**
+ * Will run on every frame render.  We are spinning the box on y-axis.
+ */
+const onRender = (scene) => {
+  if (box !== undefined) {
+    const deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+    const rpm = 10;
+    box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+  }
+};
+
+export default () => (
+  <div>
+    <SceneComponent
+      className="h-screen w-screen"
+      antialias
+      onSceneReady={onSceneReady}
+      onRender={onRender}
+      id="my-canvas"
+    />
+  </div>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
 import tsPaths from "vite-tsconfig-paths";
 
+const localPort = 5173;
+
 export default defineConfig({
   server: {
-    https: true,
-    port: 5173,
+    port: localPort,
   },
-  plugins: [
-    react(),
-    mkcert({
-      source: "coding",
-    }),
-    tsPaths(),
-  ],
+  plugins: [react(), tsPaths()],
   build: {
     cssMinify: "lightningcss",
     rollupOptions: {
